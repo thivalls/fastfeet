a79a0000ac010f5b8e8d0d995406f6e1848447c6
valls <thiago@agenciatw.com.br>
2020-03-28 23:47:06 -0300

    Configuring eslint and prettier to standardize code

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', function (req, res) {
-    res.json({ message: "Ready to go!!!" })
+routes.get('/', (req, res) => {
+  res.json({ message: 'Ready to go!!!' });
 });
 
 export default routes;
